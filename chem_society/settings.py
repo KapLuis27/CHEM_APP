8b306ba0db3de835c5abde6360861418e0be7701
KapLuis27 <113430158+KapLuis27@users.noreply.github.com>
2025-04-01 22:55:32 +0800

    host

--- chem_society/settings.py
+++ chem_society/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-63yi^u=9kxvc&r)cu&(as3woz7w%r!ljn$677rideo#zi3cq1r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['PFCS_members.onrender.com']
+ALLOWED_HOSTS = [
+    "chem-app.onrender.com",  # Replace with your Render domain
+    "127.0.0.1",
+    "localhost",
+]
 
 AUTH_USER_MODEL = 'members.Member'
 
