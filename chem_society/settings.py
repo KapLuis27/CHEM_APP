110e824a1036c505d03be387162ea13423121408
KapLuis27 <113430158+KapLuis27@users.noreply.github.com>
2025-04-01 23:05:35 +0800

    settings.py

--- chem_society/settings.py
+++ chem_society/settings.py
@@ -129,11 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'members/static')
-]
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+# Ensure STATICFILES_DIRS includes your app's static folder
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "members/static"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
